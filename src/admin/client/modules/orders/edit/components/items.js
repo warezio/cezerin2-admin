8c297749ae3fcb39ae1d489bc8ca9d6a22d3fa11
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-01-25 17:29:26 +0530

    reformat using prettier

--- src/admin/client/modules/orders/edit/components/items.js
+++ src/admin/client/modules/orders/edit/components/items.js
@@ -228,10 +228,9 @@ export class OrderItem extends React.Component {
 			<div>
 				<div className={style.item + ' row row--no-gutter middle-xs'}>
 					<div className="col-xs-2">
-						{thumbnailUrl &&
-							thumbnailUrl !== '' && (
-								<img src={thumbnailUrl} className={style.itemImage} />
-							)}
+						{thumbnailUrl && thumbnailUrl !== '' && (
+							<img src={thumbnailUrl} className={style.itemImage} />
+						)}
 					</div>
 					<div className={style.itemName + ' col-xs-4'}>
 						<Link to={`/admin/product/${item.product_id}`}>{item.name}</Link>
