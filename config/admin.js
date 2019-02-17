ffd3ed9f66f6ce147873a79dfc85222b6a4f1eaf
Luke Busstra <luke.busstra@gmail.com>
2019-02-17 19:16:10 +1000

    Add production default to API_WEB_SOCKET_URL

--- config/admin.js
+++ config/admin.js
@@ -3,6 +3,6 @@ module.exports = {
 	// dashboard UI language
 	language: process.env.LANGUAGE || 'en',
 	apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3001/api/v1',
-	apiWebSocketUrl: process.env.API_WEB_SOCKET_URL, // 'ws://localhost:3001',
+	apiWebSocketUrl: process.env.API_WEB_SOCKET_URL || '', // ws://localhost:3001 (in development)
 	developerMode: process.env.DEVELOPER_MODE || true
 };
