1f551b2f3960be0e0c9d720ccde19216eb8e625c
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-03-17 14:57:03 +0530

    Update buttons.js

--- src/modules/orderStatuses/head/components/buttons.js
+++ src/modules/orderStatuses/head/components/buttons.js
@@ -5,7 +5,6 @@ import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
 import MenuItem from 'material-ui/MenuItem';
 import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 const Fragment = React.Fragment;
 
