f469ac5031f469495a1cc618e12eaeb8b0e8d0d1
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-03-17 14:49:38 +0530

    Update description.js

--- src/modules/apps/appDetails/description.js
+++ src/modules/apps/appDetails/description.js
@@ -5,8 +5,6 @@ import style from './style.css';
 
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
-import Divider from 'material-ui/Divider';
-import FontIcon from 'material-ui/FontIcon';
 
 const AppDescription = ({
 	name,
