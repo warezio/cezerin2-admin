f44d7e91687d67b53d28cef8e80906da581242e5
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-03-17 14:35:55 +0530

    Update headButtons.js

--- src/modules/settings/paymentsEdit/components/headButtons.js
+++ src/modules/settings/paymentsEdit/components/headButtons.js
@@ -3,8 +3,6 @@ import messages from 'lib/text';
 import DeleteConfirmation from 'modules/shared/deleteConfirmation';
 import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
 
 export default class Buttons extends React.Component {
 	constructor(props) {
