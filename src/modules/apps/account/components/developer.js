297a7309c99803350fc293468aaa971b3641c869
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-03-17 14:45:10 +0530

    Update developer.js

--- src/modules/apps/account/components/developer.js
+++ src/modules/apps/account/components/developer.js
@@ -9,8 +9,6 @@ import style from './style.css';
 
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
-import Divider from 'material-ui/Divider';
-import FontIcon from 'material-ui/FontIcon';
 
 const DeveloperForm = ({
 	handleSubmit,
