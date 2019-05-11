0a45f2d1ef68e3878e2be5e90c21dc5ad7892e20
vam <vamshop@gmail.com>
2019-05-11 11:18:21 +0300

    Facebook API url fix

--- src/apps/facebook-sdk.js
+++ src/apps/facebook-sdk.js
@@ -33,7 +33,7 @@ const FACEBOOK_CODE = `<script>
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/LOCALE/sdk/xfbml.customerchat.js";
+     js.src = "https://connect.facebook.net/YOUR_LOCALE/sdk/xfbml.customerchat.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 </script>`;
