718a036c07766987a1a5fd36045b20eb8001f6a5
Luke Busstra <luke.busstra@gmail.com>
2019-06-10 21:02:56 +1000

    support drawer nav childern with drop downs Update drawer to Material-UI 4

--- src/modules/head/components/drawer.js
+++ src/modules/head/components/drawer.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import messages from 'lib/text';
 
-import Divider from 'material-ui/Divider';
-import FontIcon from 'material-ui/FontIcon';
-import Drawer from 'material-ui/Drawer';
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
+import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import Icon from '@material-ui/core/Icon';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+// Must use the icons explicidly to keep right
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import style from './style.css';
 
 const menuItems = [
 	{
@@ -60,96 +70,136 @@ const menuItems = [
 		url: '/logout',
 		icon: 'exit_to_app'
 	}
+	/*
+	Supports children nav (drop down lists)
+	Example:
+	{
+		title: "Children Example",
+		icon: 'view_agenda',
+		children: [
+			{
+				title: "child 1",
+				url: '/child1',
+				icon: 'view_agenda'
+			},
+			{
+				title: "child 2",
+				url: '/child2',
+				icon: 'calendar_today'
+			},
+		]
+	} */
 ];
 
-const styles = {
-	link: {
-		display: 'block',
-		color: 'rgba(0,0,0,0.82)',
-		textDecoration: 'none'
-	},
-	linkActive: {
-		color: 'rgb(25, 118, 210)',
-		backgroundColor: 'rgba(0,0,0,0.05)'
-	},
-	icon: {
-		left: 12,
-		color: 'rgba(0,0,0,0.54)'
-	},
-	iconActive: {
-		left: 12,
-		color: 'inherit'
-	},
-	itemInnerDiv: {
-		paddingLeft: 76,
-		fontSize: 14,
-		fontWeight: 500,
-		color: 'inherit'
-	},
-	item: {
-		color: 'inherit'
-	},
-	appBar: {
-		backgroundColor: '#fff',
-		paddingLeft: 28
-	},
-	appBarTitle: {
-		color: '#777',
-		fontSize: 18
-	},
-	menu: {
-		paddingTop: 0
-	}
-};
+const NavListItem = ({ currentUrl, item, subItem, onClick }) => (
+	<Link
+		to={item.url}
+		className={item.url === currentUrl ? style.linkActive : style.link}
+	>
+		<ListItem
+			button
+			onClick={onClick}
+			className={subItem ? style.subItemInnerDiv : style.itemInnerDiv}
+		>
+			<ListItemIcon>
+				<Icon
+					className={item.url === currentUrl ? style.iconActive : style.icon}
+				>
+					{item.icon}
+				</Icon>
+			</ListItemIcon>
+			<ListItemText primary={item.title} className={style.item} />
+		</ListItem>
+	</Link>
+);
 
-const DrawerMenu = ({ open, onClose, currentUrl }) => {
-	const items = menuItems.map((item, index) =>
-		item.title === '-' ? (
-			<Divider key={index} />
-		) : (
-			<NavLink
-				to={item.url}
-				key={index}
-				exact={true}
-				style={styles.link}
-				activeStyle={styles.linkActive}
-			>
-				<MenuItem
-					onClick={onClose}
-					primaryText={item.title}
-					innerDivStyle={styles.itemInnerDiv}
-					style={styles.item}
-					leftIcon={
-						<FontIcon
-							style={item.url === currentUrl ? styles.iconActive : styles.icon}
-							className="material-icons"
-						>
-							{item.icon}
-						</FontIcon>
-					}
-				/>
-			</NavLink>
-		)
-	);
+const SubNavListItem = ({ currentUrl, item, onClick }) => {
+	const urlSelected = item.children.some(i => i.url === currentUrl);
+	const [open, setOpen] = React.useState(urlSelected);
+
+	function subMenuClick() {
+		setOpen(!open);
+	}
 
 	return (
-		<Drawer docked={false} width={280} open={open} onRequestChange={onClose}>
-			<AppBar
-				title={messages.drawer_title}
-				style={styles.appBar}
-				titleStyle={styles.appBarTitle}
-				zDepth={0}
-				iconElementLeft={
-					<IconButton onClick={onClose}>
-						<FontIcon color="#9e9e9e" className="material-icons">
-							menu
-						</FontIcon>
-					</IconButton>
-				}
+		<div>
+			<ListItem button onClick={subMenuClick} className={style.itemInnerDiv}>
+				{item.icon && (
+					<ListItemIcon className={style.icon}>
+						<Icon>{item.icon}</Icon>
+					</ListItemIcon>
+				)}
+
+				<ListItemText primary={item.title} className={style.item} />
+
+				{open ? <ExpandLess /> : <ExpandMore />}
+			</ListItem>
+			<SubNavList
+				currentUrl={currentUrl}
+				items={item.children}
+				open={open}
+				onClick={onClick}
 			/>
-			<Menu listStyle={styles.menu} disableAutoFocus={true}>
+		</div>
+	);
+};
+
+const SubNavList = ({ currentUrl, items, onClick, open }) => (
+	<Collapse in={open} unmountOnExit>
+		<List component="div" disablePadding>
+			{items.map((item, index) => (
+				<NavListItem
+					key={item.title + index}
+					currentUrl={currentUrl}
+					item={item}
+					subItem
+					onClick={onClick}
+				/>
+			))}
+		</List>
+	</Collapse>
+);
+
+const DrawerMenu = ({ open, onClose, currentUrl }) => {
+	const items = menuItems.map((item, index) => {
+		if (item.children) {
+			return (
+				<SubNavListItem
+					key={index}
+					currentUrl={currentUrl}
+					item={item}
+					onClick={onClose}
+				/>
+			);
+		}
+		if (item.title === '-') {
+			return <Divider key={index} />;
+		}
+		return (
+			<NavListItem
+				key={index}
+				currentUrl={currentUrl}
+				item={item}
+				onClick={onClose}
+			/>
+		);
+	});
+
+	return (
+		<Drawer width="280px" anchor="left" className={style.drawer} open={open}>
+			<AppBar className={style.appBar}>
+				<Toolbar className={style.toolbar}>
+					<Typography variant="h6" className={style.appBarTitle}>
+						{messages.drawer_title}
+					</Typography>
+					<IconButton className={style.closeMenuIcon} onClick={onClose}>
+						<ChevronLeftIcon />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+			<List component="nav" className={style.menu}>
 				{items}
-			</Menu>
+			</List>
 		</Drawer>
 	);
 };
