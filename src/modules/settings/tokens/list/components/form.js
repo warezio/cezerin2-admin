f836f0cf2821d93e4ad27cade1f3d535a6ecbec0
Luke Busstra <luke.busstra@gmail.com>
2019-06-20 08:27:32 +1000

    Add revoked status to token Item Update token Item to material 4

--- src/modules/settings/tokens/list/components/form.js
+++ src/modules/settings/tokens/list/components/form.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import messages from 'lib/text';
-import Paper from 'material-ui/Paper';
-import Divider from 'material-ui/Divider';
-import FontIcon from 'material-ui/FontIcon';
-import { List, ListItem } from 'material-ui/List';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
 
 const TokenItem = ({ token }) => (
 	<div>
 		<Divider />
 		<Link
 			to={`/settings/tokens/${token.id}`}
-			style={{ textDecoration: 'none' }}
+			style={{ textDecoration: 'none', color: 'rgba(0, 0, 0, 0.84)' }}
 		>
-			<ListItem
-				rightIcon={
-					<FontIcon className="material-icons">keyboard_arrow_right</FontIcon>
-				}
-				primaryText={
-					<div className="row">
-						<div className="col-xs-6">{token.name}</div>
-						<div className="col-xs-6" style={{ color: 'rgba(0, 0, 0, 0.4)' }}>
-							{token.email}
+			<ListItem>
+				<ListItemText
+					primary={
+						<div className="row">
+							<div className="col-xs-5">{token.name}</div>
+							<div className="col-xs-5" style={{ color: 'rgba(0, 0, 0, 0.4)' }}>
+								{token.email}
+							</div>
+							<div className="col-xs-2">
+								{token.is_revoked ? messages.inactive : messages.active}
+							</div>
 						</div>
-					</div>
-				}
-			/>
+					}
+				/>
+				<ListItemSecondaryAction>
+					<IconButton edge="end">
+						<Icon>arrow_forward_ios</Icon>
+					</IconButton>
+				</ListItemSecondaryAction>
+			</ListItem>
 		</Link>
 	</div>
 );
@@ -49,7 +60,7 @@ export default class TokensList extends React.Component {
 				<div style={{ margin: 20, color: 'rgba(0, 0, 0, 0.52)' }}>
 					{messages.settings_tokenHelp}
 				</div>
-				<Paper className="paper-box" zDepth={1}>
+				<Paper className="paper-box">
 					<div style={{ width: '100%' }}>
 						<List style={{ padding: 0 }}>{listItems}</List>
 					</div>
