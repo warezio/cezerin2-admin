3da9ee1d02c85998e99731826e3862d33b600d60
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-21 08:31:29 +0700

    Update files.tsx

--- src/routes/files.tsx
+++ src/routes/files.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import List from 'modules/files/list'
+import List from '../modules/files/list'
 
 export default () => (
 	<div className="row row--no-gutter col-full-height scroll">
