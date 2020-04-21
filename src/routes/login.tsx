8822a260d501278b36d6ca26308c1347d637d30d
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-21 08:38:29 +0700

    Update login.tsx

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,8 +1,8 @@
 import React,{useState,useEffect} from 'react'
-import messages from 'lib/text'
+import messages from '../lib/text'
 import CezerinClient from 'cezerin2-client'
-import settings from 'lib/settings'
-import * as auth from 'lib/auth'
+import settings from '../lib/settings'
+import * as auth from '../lib/auth'
 
 import RaisedButton from 'material-ui/RaisedButton'
 import Paper from 'material-ui/Paper'
