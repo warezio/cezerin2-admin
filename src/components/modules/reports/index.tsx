2daa5e4bf68f076241375f55a5fc0ed5296dff5e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-03 14:23:36 +0700

    Update index.tsx

--- src/components/modules/reports/index.tsx
+++ src/components/modules/reports/index.tsx
@@ -1,63 +1,58 @@
-import React from 'react'
-import messages from 'lib/text'
-import api from 'lib/api'
-import moment from 'moment'
-import BarChart from './barChart'
-import * as utils from './utils'
+import React, { useState, useEffect } from "react"
+import messages from "lib/text"
+import api from "lib/api"
+import moment from "moment"
+import BarChart from "./barChart"
+import * as utils from "./utils"
 
-export default const OrdersBar = () => {
-	
-		
-		state = {
-			ordersData: null,
-			salesData: null,
-		}
-	}
+const OrdersBar = () => {
+  const [ordersData, setOdersData] = useState(null)
+  const [salesData, setSalesData] = useState(null)
 
-	useEffect(,[]() {
-		fetchData()
-	}
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-	fetchData = () => {
-		const filter = {
-			draft: false,
-			cancelled: false,
-			date_placed_min: moment()
-				.subtract(1, 'months')
-				.hour(0)
-				.minute(0)
-				.second(0)
-				.toISOString(),
-		}
+  const fetchData = () => {
+    const filter = {
+      draft: false,
+      cancelled: false,
+      date_placed_min: moment()
+        .subtract(1, "months")
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .toISOString(),
+    }
 
-		api.orders
-			.list(filter)
-			.then(({ status, json }) => {
-				const reportData = utils.getReportDataFromOrders(json)
-				const ordersData = utils.getOrdersDataFromReportData(reportData)
-				const salesData = utils.getSalesDataFromReportData(reportData)
-				set( ordersData, salesData })
-			})
-			.catch((error) => {
-				console.log(error)
-			})
-	}
+    api.orders
+      .list(filter)
+      .then(({ json }) => {
+        const reportData = utils.getReportDataFromOrders(json)
+        const ordersData = utils.getOrdersDataFromReportData(reportData)
+        const salesData = utils.getSalesDataFromReportData(reportData)
+        setOdersData(ordersData)
+        setSalesData(salesData)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
-	
-		const { ordersData, salesData } = state
-		return (
-			<div>
-				<BarChart
-					data={ordersData}
-					legendDisplay
-					title={messages.drawer_orders}
-				/>
-				<BarChart
-					data={salesData}
-					legendDisplay={false}
-					title={messages.salesReport}
-				/>
-			</div>
-		)
-	}
+  return (
+    <>
+      <BarChart
+        data={ordersData}
+        legendDisplay
+        title={messages.drawer_orders}
+      />
+      <BarChart
+        data={salesData}
+        legendDisplay={false}
+        title={messages.salesReport}
+      />
+    </>
+  )
 }
+
+export default OrdersBar
