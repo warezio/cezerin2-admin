c5158df5924fdc9d13d8937e8b2752482f2fd07b
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-03 12:53:16 +0700

    Moving to components

--- src/components/OldIndex/index.tsx
+++ src/components/OldIndex/index.tsx
@@ -1,8 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom"
 import "./index.css"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
 
 import { createStore, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
@@ -30,17 +26,3 @@ if (window.WebSocket) {
 } else {
   console.log("WebSocket is not supported by your browser.")
 }
-
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
