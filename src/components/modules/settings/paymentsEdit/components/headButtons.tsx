380dd47f6fc9e5bf7d0b232a8f5ba10f323139e5
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-03 14:12:07 +0700

    Update

--- src/components/modules/settings/paymentsEdit/components/headButtons.tsx
+++ src/components/modules/settings/paymentsEdit/components/headButtons.tsx
@@ -1,58 +1,53 @@
-import React from 'react'
-import messages from 'lib/text'
-import DeleteConfirmation from 'modules/shared/deleteConfirmation'
-import FontIcon from 'material-ui/FontIcon'
-import IconButton from 'material-ui/IconButton'
+import React, { useState } from "react"
+import messages from "lib/text"
+import DeleteConfirmation from "modules/shared/deleteConfirmation"
+import FontIcon from "material-ui/FontIcon"
+import IconButton from "material-ui/IconButton"
 
-export default const Buttons = () => {
-	
-		
-		state = {
-			openDelete: false,
-		}
-	}
+const Buttons = props => {
+  const [openDelete, setOpenDelete] = useState(false)
 
-	showDelete = () => {
-		set( openDelete: true })
-	}
+  const showDeletes = () => {
+    setOpenDelete(true)
+  }
 
-	closeDelete = () => {
-		set( openDelete: false })
-	}
+  const closeDelete = () => {
+    setOpenDelete(false)
+  }
 
-	deleteGroup = () => {
-		set( openDelete: false })
-		props.onDelete(props.paymentMethod.id)
-	}
+  const deleteGroup = () => {
+    setOpenDelete(false)
+    props.onDelete(props.paymentMethod.id)
+  }
 
-	
-		const { paymentMethod, onDelete } = props
-		const methodName =
-			paymentMethod && paymentMethod.name && paymentMethod.name.length > 0
-				? paymentMethod.name
-				: 'Draft'
+  const { paymentMethod } = props
+  const methodName =
+    paymentMethod && paymentMethod.name && paymentMethod.name.length > 0
+      ? paymentMethod.name
+      : "Draft"
 
-		return (
-			<span>
-				<IconButton
-					touch
-					tooltipPosition="bottom-left"
-					tooltip={messages.actions_delete}
-					onClick={showDelete}
-				>
-					<FontIcon color="#fff" className="material-icons">
-						delete
-					</FontIcon>
-				</IconButton>
-				<DeleteConfirmation
-					open={state.openDelete}
-					isSingle
-					itemsCount={1}
-					itemName={methodName}
-					onCancel={closeDelete}
-					onDelete={deleteGroup}
-				/>
-			</span>
-		)
-	}
+  return (
+    <span>
+      <IconButton
+        touch
+        tooltipPosition="bottom-left"
+        tooltip={messages.actions_delete}
+        onClick={showDelete}
+      >
+        <FontIcon color="#fff" className="material-icons">
+          delete
+        </FontIcon>
+      </IconButton>
+      <DeleteConfirmation
+        open={openDelete}
+        isSingle
+        itemsCount={1}
+        itemName={methodName}
+        onCancel={closeDelete}
+        onDelete={deleteGroup}
+      />
+    </span>
+  )
 }
+
+export default Buttons
--- src/components/modules/settings/paymentsEdit/components/selectShipping.tsx
+++ src/components/modules/settings/paymentsEdit/components/selectShipping.tsx
@@ -1,58 +1,55 @@
-import React from 'react'
-import { Field, reduxForm } from 'redux-form'
-import { List, ListItem } from 'material-ui/List'
-import Checkbox from 'material-ui/Checkbox'
+import React, { useState, useEffect } from "react"
+import { Field, reduxForm } from "redux-form"
+import { List, ListItem } from "material-ui/List"
+import Checkbox from "material-ui/Checkbox"
 
-export default const SelectShippingMethodsField = () => {
-	
-		
-		const ids = Array.isArray(props.input.value) ? props.input.value : []
-		state = {
-			selectedIds: ids,
-		}
-	}
+const SelectShippingMethodsField = props => {
+  const ids = Array.isArray(props.input.value) ? props.input.value : []
 
-	useEffect([prop.count](nextProps) {
-		const newIds = Array.isArray(nextProps.input.value)
-			? nextProps.input.value
-			: []
-		if (newIds !== state.selectedIds) {
-			set(
-				selectedIds: newIds,
-			})
-		}
-	}
+  const [selectedIds, setSelectedIDs] = useState(ids)
 
-	onCheckboxChecked = (methodId) => {
-		let ids = state.selectedIds
-		if (ids.includes(methodId)) {
-			ids = ids.filter((id) => id !== methodId)
-		} else {
-			ids.push(methodId)
-		}
-		set( selectedIds: ids })
-		props.input.onChange(ids)
-	}
+  useEffect(
+    nextProps => {
+      const newIds = Array.isArray(nextProps.input.value)
+        ? nextProps.input.value
+        : []
+      if (newIds !== selectedIds) {
+        setSelectedIds(newIds)
+      }
+    },
+    [props.count]
+  )
 
-	isCheckboxChecked = (methodId) => state.selectedIds.includes(methodId)
+  const onCheckboxChecked = methodId => {
+    let ids = selectedIds
+    if (ids.includes(methodId)) {
+      ids = ids.filter(id => id !== methodId)
+    } else {
+      ids.push(methodId)
+    }
+    setSelectedIds(ids)
+    props.input.onChange(ids)
+  }
 
-	
-		const items = props.shippingMethods.map((method) => (
-			<ListItem
-				key={method.id}
-				leftCheckbox={
-					<Checkbox
-						checked={isCheckboxChecked(method.id)}
-						onCheck={(e, isChecked) => {
-							onCheckboxChecked(method.id)
-						}}
-					/>
-				}
-				primaryText={method.name}
-				secondaryText={method.description}
-			/>
-		))
+  const isCheckboxChecked = methodId => selectedIds.includes(methodId)
 
-		return <List>{items}</List>
-	}
+  const items = props.shippingMethods.map(method => (
+    <ListItem
+      key={method.id}
+      leftCheckbox={
+        <Checkbox
+          checked={isCheckboxChecked(method.id)}
+          onCheck={() => {
+            onCheckboxChecked(method.id)
+          }}
+        />
+      }
+      primaryText={method.name}
+      secondaryText={method.description}
+    />
+  ))
+
+  return <List>{items}</List>
 }
+
+export default SelectShippingMethodsField
