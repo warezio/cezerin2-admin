a1f884b19eaa267434b9f9913c70a07e059f1138
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-03 13:41:45 +0700

    Update list.tsx

--- src/components/modules/apps/services/components/list.tsx
+++ src/components/modules/apps/services/components/list.tsx
@@ -1,53 +1,52 @@
-import React,{useEffect} from 'react'
-import { Link } from 'gatsby'
-import messages from 'lib/text'
-import apps from 'src/apps'
-import RaisedButton from 'material-ui/RaisedButton'
-import ServiceItem from './serviceItem'
-import AppItem from './appItem'
-import './style.sass'
+import React, { useEffect } from "react"
+import { Link } from "gatsby"
+import messages from "lib/text"
+import apps from "src/apps"
+import RaisedButton from "material-ui/RaisedButton"
+import ServiceItem from "./serviceItem"
+import AppItem from "./appItem"
+import "./style.sass"
 
-const ServicesList = (props) => {
-	useEffect(() => props.fetchData(),[])
+const ServicesList = props => {
+  useEffect(() => props.fetchData(), [])
 
-		const { services, webstoreAuthorized } = props
+  const { services, webstoreAuthorized } = props
 
-		let serviceItems = null
-		if (services && services.data) {
-			serviceItems = services.data.map((service, index) => (
-				<ServiceItem key={index} service={service} />
-			))
-		}
+  let serviceItems = null
+  if (services && services.data) {
+    serviceItems = services.data.map((service, index) => (
+      <ServiceItem key={index} service={service} />
+    ))
+  }
 
-		const appItems = apps.map((app, index) => (
-			<AppItem key={index} app={app.Description} />
-		))
+  const appItems = apps.map((app, index) => (
+    <AppItem key={index} app={app.Description} />
+  ))
 
-		return (
-			<div
-				className="row row--no-gutter scroll col-full-height"
-				style={{ padding: 20, alignContent: 'flex-start' }}
-			>
-				{appItems}
-				{!webstoreAuthorized && (
-					<div
-						style={{
-							width: '100%',
-							marginTop: 30,
-							color: 'rgba(0, 0, 0, 0.52)',
-						}}
-					>
-						{messages.loadFromWebstore}
-						&nbsp;&nbsp;
-						<Link to="/apps/login">
-							<RaisedButton label={messages.loginTitle} />
-						</Link>
-					</div>
-				)}
-				{serviceItems}
-			</div>
-		)
-	}
+  return (
+    <div
+      className="row row--no-gutter scroll col-full-height"
+      style={{ padding: 20, alignContent: "flex-start" }}
+    >
+      {appItems}
+      {!webstoreAuthorized && (
+        <div
+          style={{
+            width: "100%",
+            marginTop: 30,
+            color: "rgba(0, 0, 0, 0.52)",
+          }}
+        >
+          {messages.loadFromWebstore}
+          &nbsp;&nbsp;
+          <Link to="/apps/login">
+            <RaisedButton label={messages.loginTitle} />
+          </Link>
+        </div>
+      )}
+      {serviceItems}
+    </div>
+  )
 }
 
 export default ServicesList
