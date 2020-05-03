1a979d1c9b7fd98215ac745e23ee7856483da86e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-03 13:01:42 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,50 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    // sitemap
+    `gatsby-plugin-sitemap`,
+    // Robots.txt
+    "gatsby-plugin-robots-txt",
+    // sass/scss
+    `gatsby-plugin-sass`,
+    // Page Load screen
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
+    },
+    // typescript
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `React`, // was jsx defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    // redux
+    {
+      resolve: `gatsby-plugin-react-redux`,
+      options: {
+        // [required] - path to your createStore module
+        pathToCreateStoreModule: "./src/state/createStore",
+        // [optional] - options passed to `serialize-javascript`
+        // info: https://github.com/yahoo/serialize-javascript#options
+        // will be merged with these defaults:
+        serialize: {
+          space: 0,
+          isJSON: true,
+          unsafe: false,
+        },
+        // [optional] - if true will clean up after itself on the client, default:
+        cleanupOnClient: true,
+        // [optional] - name of key on `window` where serialized state will be stored, default:
+        windowKey: "__PRELOADED_STATE__",
+      },
+    },
   ],
 }
