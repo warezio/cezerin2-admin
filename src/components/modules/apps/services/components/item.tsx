0a28db660f82981796842a70285fabd97fe47d9d
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-03 13:39:42 +0700

    Update item.tsx

--- src/components/modules/apps/services/components/item.tsx
+++ src/components/modules/apps/services/components/item.tsx
@@ -1,71 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Card, CardMedia, CardTitle } from 'material-ui/Card'
-import FontIcon from 'material-ui/FontIcon'
-import './style.sass'
+import React from "react"
+import { Link } from "gatsby"
+import { Card, CardMedia, CardTitle } from "material-ui/Card"
+import FontIcon from "material-ui/FontIcon"
+import "./style.sass"
 
 const styles = {
-	card: {
-		width: 280,
-		marginBottom: 15,
-		marginRight: 15,
-	},
-	textContainer: {
-		paddingBottom: 0,
-	},
-	title: {
-		color: '#212121',
-		fontSize: '15px',
-		lineHeight: '18px',
-		overflow: 'hidden',
-		textOverflow: 'ellipsis',
-		whiteSpace: 'nowrap',
-	},
-	subtitle: {
-		color: '#616161',
-		fontSize: '13px',
-		lineHeight: '16px',
-		overflow: 'hidden',
-		textOverflow: 'ellipsis',
-		whiteSpace: 'nowrap',
-		marginTop: 5,
-	},
-	link: {
-		textDecoration: 'none',
-	},
+  card: {
+    width: 280,
+    marginBottom: 15,
+    marginRight: 15,
+  },
+  textContainer: {
+    paddingBottom: 0,
+  },
+  title: {
+    color: "#212121",
+    fontSize: "15px",
+    lineHeight: "18px",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+  },
+  subtitle: {
+    color: "#616161",
+    fontSize: "13px",
+    lineHeight: "16px",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    marginTop: 5,
+  },
+  link: {
+    textDecoration: "none",
+  },
 }
 
 const Item = ({ path, coverUrl, title, developer, enabled }) => (
-	<Link to={path} style={styles.link}>
-		<Card
-			style={styles.card}
-			containerStyle={styles.textContainer}
-			className="card}
-		>
-			<CardMedia
-				className="servicesCover}
-				style={{ backgroundImage: `url(${coverUrl})` }}
-			/>
-			<CardTitle
-				title={title}
-				subtitle={
-					<div>
-						{developer}
-						{enabled && (
-							<FontIcon
-								style={{ color: '#FF9900', float: 'right' }}
-								className="material-icons"
-							>
-								check_circle
-							</FontIcon>
-						)}
-					</div>
-				}
-				titleStyle={styles.title}
-				subtitleStyle={styles.subtitle}
-			/>
-		</Card>
-	</Link>
+  <Link to={path} style={styles.link}>
+    <Card
+      style={styles.card}
+      containerStyle={styles.textContainer}
+      className="card"
+    >
+      <CardMedia
+        className="servicesCover"
+        style={{ backgroundImage: `url(${coverUrl})` }}
+      />
+      <CardTitle
+        title={title}
+        subtitle={
+          <>
+            {developer}
+            {enabled && (
+              <FontIcon
+                style={{ color: "#FF9900", float: "right" }}
+                className="material-icons"
+              >
+                check_circle
+              </FontIcon>
+            )}
+          </>
+        }
+        titleStyle="title"
+        subtitleStyle="subtitle"
+      />
+    </Card>
+  </Link>
 )
 
 export default Item
